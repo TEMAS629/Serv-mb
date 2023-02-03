e96bdbc85f32a0963650e221f283c754533dfd4e
TEMAS629 <94691533+TEMAS629@users.noreply.github.com>
2023-02-03 13:42:26 +0300

    Add files via upload

--- Kirillov/index.js
+++ Kirillov/index.js
@@ -1,67 +1,21 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const host = '127.0.0.1'
-const port = 7000
-
-function notFound(res) {
-  res.statusCode = 404
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Not found\n')
-}
-
-const server = http.createServer((req, res) => {
-  switch (req.method) {
-    case 'GET': {
-      switch (req.url) {
-        case '/home': {
-          res.statusCode = 200
-          res.setHeader('Content-Type', 'text/plain')
-          res.end('Home page\n')
-          break
-        }
-        case '/about': {
-          res.statusCode = 200
-          res.setHeader('Content-Type', 'text/plain')
-          res.end('About page\n')
-          break
-        }
-        default: {
-          notFound(res)
-          break
-        }
-      }
-
-      break
-    }
-    case 'POST': {
-      switch (req.url) {
-        case '/api/admin': {
-          res.statusCode = 200
-          res.setHeader('Content-Type', 'text/plain')
-          res.end('Create admin request\n')
-          break
-        }
-        case '/api/user': {
-          res.statusCode = 200
-          res.setHeader('Content-Type', 'text/plain')
-          res.end('Create user request\n')
-          break
-        }
-        default: {
-          notFound(res)
-          break
-        }
-      }
-
-      break
-    }
-    default: {
-      notFound(res)
-      break
-    }
-  }
+app.use('/about', function (request, response) {
+  response.send('О сайте')
 })
 
-server.listen(port, host, () => {
-  console.log(`Server listens http://${host}:${port}`)
+app.use('/products/create', function (request, response) {
+  response.send('Добавление товара')
 })
+app.use('/products/:id', function (request, response) {
+  response.send(`Товар ${request.params.id}`)
+})
+app.use('/products/', function (request, response) {
+  response.send('Список товаров')
+})
+
+app.use('/', function (request, response) {
+  response.send('Главная страница')
+})
+app.listen(7000)
